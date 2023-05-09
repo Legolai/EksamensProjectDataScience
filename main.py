e7072a51e89142fb270543c9d7857d7c815e57d2
Legolai <Nico360f@gmail.com>
2023-05-09 13:26:16 +0200

    update

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import cv2
 
 
-def main():
-    print("Hello World!")
+def main() -> None:
+    """Main function of the program."""
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 420)
+
+    cv2.CascadeClassifier()
 
 
 if __name__ == "__main__":
--- stock.py
+++
@@ -1,5 +0,0 @@
-
-class Stock:
-
-    def __init__(self) -> None:
-        pass
